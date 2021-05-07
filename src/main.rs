c42c705d257dc8bfb5b9ad131c98dc2157c9c83b
koyashiro <develop@koyashiro.net>
2021-05-08 01:15:55 +0900

    feat: implement `genec`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,72 @@
+use std::fs;
+use std::io;
+
+use anyhow::Result;
+
 mod editorconfig;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    let path = ".editorconfig";
+
+    let mut config = editorconfig::Config::new("*");
+    config.indent_style = editorconfig::IndentStyle::Space;
+    config.indent_size = Some(2);
+    config.end_of_line = editorconfig::EndOfLine::LF;
+    config.charset = editorconfig::Charset::UTF8;
+    config.trim_trailing_whitespace = Some(true);
+    config.insert_final_newline = Some(true);
+    let mut editor_config = editorconfig::EditorConfig::new();
+    editor_config.configs.push(config);
+
+    let f = fs::File::create(&path)?;
+    let mut w = io::BufWriter::new(f);
+    write_editor_config(&mut w, &editor_config)?;
+
+    Ok(())
+}
+
+fn write_editor_config(
+    w: &mut impl io::Write,
+    editor_config: &editorconfig::EditorConfig,
+) -> Result<()> {
+    writeln!(w, "{}", editor_config)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod genec_test {
+    use super::*;
+
+    #[test]
+    fn write_editor_config_test() -> Result<()> {
+        let mut config = editorconfig::Config::new("*");
+        config.indent_style = editorconfig::IndentStyle::Space;
+        config.indent_size = Some(2);
+        config.end_of_line = editorconfig::EndOfLine::LF;
+        config.charset = editorconfig::Charset::UTF8;
+        config.trim_trailing_whitespace = Some(true);
+        config.insert_final_newline = Some(true);
+
+        let mut editor_config = editorconfig::EditorConfig::new();
+        editor_config.configs.push(config);
+
+        let mut w = Vec::new();
+        write_editor_config(&mut w, &editor_config)?;
+        assert_eq!(
+            w,
+            b"\
+root = true
+
+[*]
+indent_style = space
+indent_size = 2
+end_of_line = lf
+charset = utf-8
+trim_trailing_whitespace = true
+insert_final_newline = true
+"
+        );
+
+        Ok(())
+    }
 }
