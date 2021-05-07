b6d7b0ad224aa4a969216429a24923fb879f78d4
koyashiro <develop@koyashiro.net>
2021-05-08 04:07:24 +0900

    feat: implement `genec::config::config_path()` and `genec::config::config_file()`

--- src/genec.rs
+++ src/genec.rs
@@ -4,3 +4,32 @@ use crate::editorconfig::Config;
 
 #[allow(dead_code)]
 pub type ConfigSet = HashMap<String, Config>;
+
+pub mod config {
+    use std::{fs::File, path::PathBuf};
+
+    use anyhow::{Context, Result};
+    use dirs::config_dir;
+
+    #[allow(dead_code)]
+    pub const CONFIG_ENV: &str = "GENEC_CONFIG";
+
+    #[allow(dead_code)]
+    pub struct Config {
+        base_config_path: String,
+    }
+
+    #[allow(dead_code)]
+    pub fn config_path() -> Result<PathBuf> {
+        let config_dir = config_dir().context("failed to get config dir")?;
+        let config_path = config_dir.join("genec").join("config");
+        Ok(config_path)
+    }
+
+    #[allow(dead_code)]
+    pub fn config_file() -> Result<File> {
+        let config_path = config_path()?;
+        let file = File::create(config_path)?;
+        Ok(file)
+    }
+}
